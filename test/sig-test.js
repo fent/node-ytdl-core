c58e4b7dbe78f1314032c2f32eaffe2e5527cb5b
Roly Fentanes <roly426@gmail.com>
2014-03-29 15:57:40 -0400

    extract sig when swapHeadWithBody() inside of func body

--- test/sig-test.js
+++ test/sig-test.js
@@ -11,6 +11,8 @@ var file3 = fs.readFileSync(path.resolve(
   __dirname, 'files/html5player/html5player-vflG49soT.js'), 'utf8');
 var file4 = fs.readFileSync(path.resolve(
   __dirname, 'files/html5player/html5player-vflcY_8N0.js'), 'utf8');
+var file5 = fs.readFileSync(path.resolve(
+  __dirname, 'files/html5player/html5player-en_US-vflnwMARr.js'), 'utf8');
 
 
 describe('Signature decypher', function() {
@@ -18,10 +20,12 @@ describe('Signature decypher', function() {
   var tokens2 = ['w21', 'w3', 's1', 'r', 'w44', 'w36', 'r', 'w41', 's1'];
   var tokens3 = ['w32', 'r', 's3', 'r', 's1', 'r', 'w19', 'w24', 's3'];
   var tokens4 = ['s2', 'w36', 's1', 'r', 'w18', 'r', 'w19', 'r'];
+  var tokens5 = ['s3', 'r', 'w24', 's2'];
 
   describe('extract decyphering actions', function() {
     it('Returns the correct set of actions', function() {
-      var actions = sig.extractActions(file1);
+      var actions;
+      actions = sig.extractActions(file1);
       assert.deepEqual(actions, tokens1);
       actions = sig.extractActions(file2);
       assert.deepEqual(actions, tokens2);
@@ -29,6 +33,8 @@ describe('Signature decypher', function() {
       assert.deepEqual(actions, tokens3);
       actions = sig.extractActions(file4);
       assert.deepEqual(actions, tokens4);
+      actions = sig.extractActions(file5);
+      assert.deepEqual(actions, tokens5);
     });
   });
 
