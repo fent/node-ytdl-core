9614b3063106b8f3a7e867cacec65ac9e7a2dedf
fent <933490+fent@users.noreply.github.com>
2014-07-16 20:59:23 -0400

    another file to extract signature from

--- test/sig-test.js
+++ test/sig-test.js
@@ -5,16 +5,21 @@ var path   = require('path');
 
 var file1 = fs.readFileSync(path.resolve(
   __dirname, 'files/html5player/html5player-en_US-vfl0Cbn9e.js'), 'utf8');
+var file2 = fs.readFileSync(path.resolve(
+  __dirname, 'files/html5player/html5player-en_US-vfl5aDZwb.js'), 'utf8');
 
 
 describe('Signature decypher', function() {
   var tokens1 = ['w15', 'w44', 'r', 'w24', 's3', 'r', 'w2', 'w50'];
+  var tokens2 = ['w15', 'w44', 'r', 'w24', 's3', 'r', 'w2', 'w50'];
 
   describe('extract decyphering actions', function() {
     it('Returns the correct set of actions', function() {
       var actions;
       actions = sig.extractActions(file1);
       assert.deepEqual(actions, tokens1);
+      actions = sig.extractActions(file2);
+      assert.deepEqual(actions, tokens2);
     });
   });
 
