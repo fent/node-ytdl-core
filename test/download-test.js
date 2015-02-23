47049c6ce6bc68e13afa3880b6550d31d309f937
Roly Fentanes <roly426@gmail.com>
2015-02-22 19:56:35 -0500

    get info from video watch page, making one less request when downloading

--- test/download-test.js
+++ test/download-test.js
@@ -7,23 +7,23 @@ var streamEqual = require('stream-equal');
 var ytdl        = require('..');
 
 var YT_HOST = 'http://www.youtube.com';
-var INFO_PATH = '/get_video_info?hl=en_US&el=detailpage&video_id=';
-var VIDEO_BASE = 'http://www.youtube.com/watch?v=';
+var VIDEO_PATH = '/watch?v=';
+var VIDEO_BASE = 'http://www.youtube.com' + VIDEO_PATH;
 
 
 describe('Download video', function() {
   it('Should be pipeable and data equal to stored file', function(done) {
     var id = '_HSylqgVYQI';
     var link = VIDEO_BASE + id;
-    var page = path.resolve(__dirname, 'files/video_info/' + id);
+    var page = path.resolve(__dirname, 'files/watch/' + id);
     var video = path.resolve(__dirname, 'files/video/' + id + '.flv');
     var filter = function(format) { return format.container === 'mp4'; };
 
     nock(YT_HOST)
-      .get(INFO_PATH + id)
+      .get(VIDEO_PATH + id)
       .replyWithFile(200, page);
 
-    var format = require('./files/info/' + id + '_download.json')
+    var format = require('./files/info/' + id + '.json')
       .formats.filter(filter)[0];
     var parsed = url.parse(format.url);
     var scope = nock(parsed.protocol + '//' + parsed.host)
