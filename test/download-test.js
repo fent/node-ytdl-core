7e0c2b9c9d51ac601a93171827070e295bfb11f2
fent <933490+fent@users.noreply.github.com>
2015-10-25 03:20:04 -0400

    added test for 302 redirect

--- test/download-test.js
+++ test/download-test.js
@@ -1,6 +1,7 @@
 var assert      = require('assert');
 var path        = require('path');
 var fs          = require('fs');
+var url         = require('url');
 var streamEqual = require('stream-equal');
 var nock        = require('./nock');
 var ytdl        = require('..');
@@ -9,12 +10,12 @@ var VIDEO_BASE = 'https://www.youtube.com/watch?v=';
 
 
 describe('Download video', function() {
-  it('Should be pipeable and data equal to stored file', function(done) {
-    var id = '_HSylqgVYQI';
-    var link = VIDEO_BASE + id;
-    var video = path.resolve(__dirname, 'files/' + id + '/video.flv');
-    var filter = function(format) { return format.container === 'mp4'; };
+  var id = '_HSylqgVYQI';
+  var link = VIDEO_BASE + id;
+  var video = path.resolve(__dirname, 'files/' + id + '/video.flv');
+  var filter = function(format) { return format.container === 'mp4'; };
 
+  it('Should be pipeable and data equal to stored file', function(done) {
     var scope1 = nock(id, { dashmpd: true });
     var stream = ytdl(link, { filter: filter });
 
@@ -34,6 +35,38 @@ describe('Download video', function() {
       assert.ok(infoEmitted);
       assert.ok(equal);
       done();
+    });
+  });
+
+  describe('that redirects', function() {
+    it('Should download file after redirect', function(done) {
+      var id = '_HSylqgVYQI';
+      var link = VIDEO_BASE + id;
+      var video = path.resolve(__dirname, 'files/' + id + '/video.flv');
+      var filter = function(format) { return format.container === 'mp4'; };
+
+      var scope1 = nock(id, { dashmpd: true });
+      var stream = ytdl(link, { filter: filter });
+
+      var scope2, scope3;
+      stream.on('info', function(info, format) {
+        var parsed = url.parse(format.url);
+        scope2 = nock.nock(parsed.protocol + '//' + parsed.host)
+          .get(parsed.path)
+          .reply(302, '', { Location: 'http://somehost.com/somefile.mp4' });
+        scope3 = nock.url('http://somehost.com/somefile.mp4', video);
+      });
+
+      var filestream = fs.createReadStream(video);
+      streamEqual(filestream, stream, function(err, equal) {
+        scope1.done();
+        scope2.done();
+        scope3.done();
+        if (err) return done(err);
+
+        assert.ok(equal);
+        done();
+      });
     });
   });
 });
