e6774ce2e0534ce5bf9263833cf53ce24c2058dd
Roly Fentanes <roly426@gmail.com>
2017-02-21 01:20:48 -0500

    more test coverage

--- test/sig-test.js
+++ test/sig-test.js
@@ -3,7 +3,7 @@ var assert = require('assert-diff');
 var fs     = require('fs');
 var path   = require('path');
 var nock   = require('./nock');
-var sinon  = require('sinon');
+var spy    = require('sinon').spy;
 var muk    = require('muk-prop');
 
 var html5player = require('./html5player.json');
@@ -51,10 +51,8 @@ describe('Get tokens', function() {
   });
 
   describe('Unable to find key in filename', function() {
-    var warn = sinon.spy();
-    before(function() {
-      muk(console, 'warn', warn);
-    });
+    var warn = spy();
+    muk(console, 'warn', warn);
     after(muk.restore);
     it('Warns the console, still attempts to get tokens', function(done) {
       var url = '//s.ytimg.com/badfilename.js';
@@ -153,11 +151,65 @@ describe('Signature decipher', function() {
   });
 });
 
-describe('Decipher formats', function() {
-  var formats = [
-
-  ];
+describe('Set download URL', function() {
   it('Adds signature to download URL', function() {
+    var format = {
+      fallback_host: 'tc.v9.cache7.googlevideo.com',
+      quality: 'small',
+      type: 'video/x-flv',
+      itag: '5',
+      url: 'https://r4---sn-p5qlsnsr.googlevideo.com/videoplayback?nh=IgpwZjAxLmlhZDI2Kgw3Mi4xNC4yMDMuOTU&upn=utAH1aBebVk&source=youtube&sparams=cwbhb%2Cdur%2Cid%2Cinitcwndbps%2Cip%2Cipbits%2Citag%2Clmt%2Cmime%2Cmm%2Cmn%2Cms%2Cmv%2Cnh%2Cpl%2Crequiressl%2Csource%2Cupn%2Cexpire&initcwndbps=772500&pl=16&ip=0.0.0.0&lmt=1309008098017854&key=yt6&id=o-AJj1D_OYO_EieAH08Qa2tRsP6zid9dsuPAvktizyDRlv&expire=1444687469&mm=31&mn=sn-p5qlsnsr&itag=5&mt=1444665784&mv=m&cwbhb=yes&fexp=9408208%2C9408490%2C9408710%2C9409069%2C9414764%2C9415435%2C9416126%2C9417224%2C9417380%2C9417488%2C9417707%2C9418448%2C9418494%2C9419445%2C9419802%2C9420324%2C9420348%2C9420982%2C9421013%2C9421170%2C9422341%2C9422540&ms=au&sver=3&dur=298.109&requiressl=yes&ipbits=0&mime=video%2Fx-flv&ratebypass=yes',
+      container: 'flv',
+      resolution: '240p',
+      encoding: 'Sorenson H.283',
+      profile: null,
+      bitrate: '0.25',
+      audioEncoding: 'mp3',
+      audioBitrate: 64
+    };
+    sig.setDownloadURL(format, 'mysiggy', false);
+    assert.ok(format.url.indexOf('signature=mysiggy') > -1);
+  });
 
+  describe('With a badly formatted URL', function() {
+    var format = {
+      url: 'https://r4---sn-p5qlsnsr.googlevideo.com/videoplayback?%',
+    };
+
+    it('Does not set URL', function() {
+      sig.setDownloadURL(format, 'mysiggy', false);
+      assert.ok(format.url.indexOf('signature=mysiggy') === -1);
+    });
+
+    describe('With debug on', function() {
+      it('Logs to console', function() {
+        var warn = spy();
+        muk(console, 'warn', warn);
+        after(muk.restore);
+        sig.setDownloadURL(format, 'mysiggy', true);
+        assert.ok(warn.called);
+        assert.ok(format.url.indexOf('signature=mysiggy') === -1);
+      });
+    });
+  });
+
+  describe('Without a URL', function() {
+    var format = { bla: 'blu' };
+
+    it('Does not set URL', function() {
+      sig.setDownloadURL(format, 'nothing', false);
+      assert.deepEqual(format, { bla: 'blu' });
+    });
+
+    describe('With debug on', function() {
+      it('Logs to console', function() {
+        var warn = spy();
+        muk(console, 'warn', warn);
+        after(muk.restore);
+        sig.setDownloadURL(format, 'nothing', true);
+        assert.ok(warn.called);
+        assert.deepEqual(format, { bla: 'blu' });
+      });
+    });
   });
 });
