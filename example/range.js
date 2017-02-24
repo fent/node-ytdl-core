ad928d3b209c7cfb243d0d115d21f87494f43db7
fent <933490+fent@users.noreply.github.com>
2017-02-24 07:57:26 -0500

    add range example (#87)

---
+++ example/range.js
@@ -0,0 +1,14 @@
+var path = require('path');
+var fs   = require('fs');
+var ytdl = require('..');
+
+var url = 'https://www.youtube.com/watch?v=CehAKQL463M';
+var output = path.resolve(__dirname, 'video.mp4');
+
+var video = ytdl(url, { range: '0-1000' });
+video.pipe(fs.createWriteStream(output));
+
+video.on('end', function() {
+  ytdl(url, { range: '1001-' })
+    .pipe(fs.createWriteStream(output, { flags: 'a' }));
+});
