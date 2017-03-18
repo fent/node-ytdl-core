72644a4ddabdf314b9c29a2ab532d4cf9fd33322
TimeForANinja <t.kutscha@yahoo.de>
2017-03-18 21:50:46 +0100

    adding support for range back in

--- example/range.js
+++ example/range.js
@@ -5,7 +5,7 @@ var ytdl = require('..');
 var url = 'https://www.youtube.com/watch?v=CehAKQL463M';
 var output = path.resolve(__dirname, 'video.mp4');
 
-var video = ytdl(url, { range: '0-1000' });
+var video = ytdl(url, { range: { start: 0, end: 1000 } });
 video.pipe(fs.createWriteStream(output));
 
 video.on('end', function() {
