b4e288a502fa3a0e7838e0396cac74e267e3505a
fent <933490+fent@users.noreply.github.com>
2017-05-06 22:20:47 -0400

    a bit more logging to discord example

--- example/discord.js
+++ example/discord.js
@@ -5,8 +5,13 @@ const ytdl    = require('..');
 const client = new Discord.Client();
 client.login(' Y o u r   B o t   T o k e n ');
 
+client.on('ready', () => {
+  console.log('discord.js client ready');
+});
+
 client.on('message', message => {
   if (message.content.startsWith('++play')) {
+    console.log('Got a song request!');
     const voiceChannel = message.member.voiceChannel;
     if (!voiceChannel) {
       return message.reply('Please be in a voice channel first!');
@@ -14,7 +19,7 @@ client.on('message', message => {
     voiceChannel.join()
       .then(connnection => {
         let stream = ytdl('https://www.youtube.com/watch?v=dQw4w9WgXcQ', {
-          filter : 'audioonly'
+          filter : 'audioonly',
         });
         const dispatcher = connnection.playStream(stream);
         dispatcher.on('end', () => {
