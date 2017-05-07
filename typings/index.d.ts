1a7911cc5b8c14722f1dc141bef96cc1eb46412c
Roly Fentanes <roly426@gmail.com>
2017-05-06 22:38:17 -0400

    update typings

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -4,7 +4,7 @@ declare module 'ytdl-core' {
 
   namespace ytdl {
     type downloadOptions = {
-      quality?: 'lowest' | 'highest';
+      quality?: 'lowest' | 'highest' | string | number;
       filter?: 'video' | 'videoonly' | 'audio' | 'audioonly' | ((format: videoFormat) => boolean);
       format?: videoFormat;
       range?: {
@@ -12,7 +12,6 @@ declare module 'ytdl-core' {
         end?: number;
       };
       requestOptions?: {};
-      request?: (url: string, options: {}, callback?: (err: Error, body: any) => void) => ClientRequest;
     }
 
     type videoFormat = {
