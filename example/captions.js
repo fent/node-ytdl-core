f32da2b5ae6f3faac1dc925c088b3ed07a010ae1
Roly Fentanes <roly426@gmail.com>
2017-07-24 22:42:10 -0400

    Parse `info.player_response`, add captions example (closes #191)

---
+++ example/captions.js
@@ -0,0 +1,38 @@
+const fs    = require('fs');
+const path  = require('path');
+const https = require('https');
+const ytdl  = require('..');
+const id    = 'https://www.youtube.com/watch?v=QRS8MkLhQmM';
+const lang  = 'en';
+
+ytdl.getInfo(id, (err, info) => {
+  if (err) throw err;
+  var tracks = info
+    .player_response.captions
+    .playerCaptionsTracklistRenderer.captionTracks;
+  if (tracks && tracks.length) {
+    console.log('Found captions for',
+      tracks.map(t => t.name.simpleText).join(', '));
+    var track = tracks.find(t => t.languageCode === lang);
+    if (track) {
+      console.log('Retrieving captions:', track.name.simpleText);
+      console.log('URL', track.baseUrl);
+      var output = [
+        info.title,
+        track.languageCode,
+        'xml',
+      ].join('.');
+      console.log('Saving to', output);
+      https.get(track.baseUrl, (res) => {
+        res.pipe(fs.createWriteStream(path.resolve(__dirname, output)));
+      });
+
+    } else {
+      console.log('Could not find captions for', lang);
+
+    }
+  } else {
+    console.log('No captions found for this video');
+  }
+
+});
