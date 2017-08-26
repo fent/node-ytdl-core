818011f589506af8bde0436a9e5c750f0ee144c5
Roly Fentanes <roly426@gmail.com>
2017-08-26 15:12:51 -0400

    make ip regex for robust

--- example/get_info.js
+++ example/get_info.js
@@ -11,7 +11,7 @@ ytdl.getInfo(id, (err, info) => {
   console.log('rating:', info.average_rating);
   console.log('uploaded by:', info.author.name);
   const json = JSON.stringify(info, null, 2)
-    .replace(/(ip(?:=|%3D))((?:(?:25[0-5]|2[0-4]\d|[01]?\d\d?)\.){3}(?:25[0-5]|2[0-4]\d|[01]?\d\d?))/g, '$10.0.0.0');
+    .replace(/(ip(?:=|%3D|\/))((?:(?:25[0-5]|2[0-4]\d|[01]?\d\d?)\.){3}(?:25[0-5]|2[0-4]\d|[01]?\d\d?)|[0-9a-f]{1,4}(?:(?::|%3A)[0-9a-f]{1,4}){7})/ig, '$10.0.0.0');
   fs.writeFile(filepath, json, err => {
     if (err) throw err;
   });
