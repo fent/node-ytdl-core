facf2991d5bd4a0e21ed5c87fc0fba6355e8d4a2
fent <933490+fent@users.noreply.github.com>
2018-06-23 22:33:54 -0400

    refactor: move parse-time to m3u8stream

--- test/util-test.js
+++ test/util-test.js
@@ -572,25 +572,3 @@ describe('util.parallel()', () => {
     });
   });
 });
-
-
-describe('util.fromHumanTime', () => {
-  it('Time format 00:00:00.000', () => {
-    assert.equal(util.fromHumanTime('25.000'), 25000);
-    assert.equal(util.fromHumanTime('05:30'), 60000 * 5 + 30000);
-    assert.equal(util.fromHumanTime('01:05:30'), 60000 * 60 + 60000 * 5 + 30000);
-    assert.equal(util.fromHumanTime('1:30.123'), 60000 + 30000 + 123);
-  });
-
-  it('Time format 0ms, 0s, 0m, 0h', () => {
-    assert.equal(util.fromHumanTime('2ms'), 2);
-    assert.equal(util.fromHumanTime('1m'), 60000);
-    assert.equal(util.fromHumanTime('1m10s'), 60000 + 10000);
-    assert.equal(util.fromHumanTime('2hm10s500ms'), 3600000 * 2 + 10000 + 500);
-  });
-
-  it('No format', () => {
-    assert.equal(util.fromHumanTime('1000'), 1000);
-    assert.equal(util.fromHumanTime(200), 200);
-  });
-});
