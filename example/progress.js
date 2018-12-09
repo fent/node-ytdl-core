ab7b591321aff8004f81a72b8ab590a86f7cf931
fent <933490+fent@users.noreply.github.com>
2018-12-09 03:56:42 -0500

    docs(example): minor variable name

--- example/progress.js
+++ example/progress.js
@@ -13,13 +13,13 @@ video.once('response', () => {
   starttime = Date.now();
 });
 video.on('progress', (chunkLength, downloaded, total) => {
-  const floatDownloaded = downloaded / total;
+  const percent = downloaded / total;
   const downloadedMinutes = (Date.now() - starttime) / 1000 / 60;
   readline.cursorTo(process.stdout, 0);
-  process.stdout.write(`${(floatDownloaded * 100).toFixed(2)}% downloaded`);
+  process.stdout.write(`${(percent * 100).toFixed(2)}% downloaded`);
   process.stdout.write(`(${(downloaded / 1024 / 1024).toFixed(2)}MB of ${(total / 1024 / 1024).toFixed(2)}MB)\n`);
   process.stdout.write(`running for: ${downloadedMinutes.toFixed(2)}minutes`);
-  process.stdout.write(`, estimated time left: ${(downloadedMinutes / floatDownloaded - downloadedMinutes).toFixed(2)}minutes `);
+  process.stdout.write(`, estimated time left: ${(downloadedMinutes / percent - downloadedMinutes).toFixed(2)}minutes `);
   readline.moveCursor(process.stdout, 0, -1);
 });
 video.on('end', () => {
