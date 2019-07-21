df9ef60d143893226d81f2c6bc9c3dcdf85166b9
fent <933490+fent@users.noreply.github.com>
2019-07-21 09:57:39 -0400

    docs(example): on `ffmpeg.js` example, print progress when downloading audio

--- example/ffmpeg.js
+++ example/ffmpeg.js
@@ -1,4 +1,5 @@
 // Install fluent-ffmpeg before running this!
+const readline = require('readline');
 const path   = require('path');
 const fs     = require('fs');
 const ytdl   = require('..');
@@ -8,11 +9,21 @@ const url = 'https://www.youtube.com/watch?v=TGbwL8kSpEk';
 const audioOutput = path.resolve(__dirname, 'sound.mp4');
 const mainOutput = path.resolve(__dirname, 'output.mp4');
 
-ytdl(url, { filter: format => {
-  return format.container === 'm4a' && !format.encoding; } })
+console.log('downloading audio track');
+ytdl(url, {
+  filter: format => format.container === 'm4a' && !format.encoding
+}).on('error', console.error)
+  .on('progress', (chunkLength, downloaded, total) => {
+    const percent = downloaded / total;
+    readline.cursorTo(process.stdout, 0);
+    process.stdout.write(`${(percent * 100).toFixed(2)}% downloaded `);
+    process.stdout.write(`(${(downloaded / 1024 / 1024).toFixed(2)}MB of ${(total / 1024 / 1024).toFixed(2)}MB)`);
+  })
+
   // Write audio to file since ffmpeg supports only one input stream.
   .pipe(fs.createWriteStream(audioOutput))
   .on('finish', () => {
+    console.log('\ndownloading video');
     ffmpeg()
       .input(ytdl(url, { filter: format => {
         return format.container === 'mp4' && !format.audioEncoding; } }))
@@ -22,8 +33,8 @@ ytdl(url, { filter: format => {
       .save(mainOutput)
       .on('error', console.error)
       .on('progress', progress => {
-        process.stdout.cursorTo(0);
-        process.stdout.clearLine(1);
+        readline.cursorTo(process.stdout, 0);
+        readline.clearLine(process.stdout, 1);
         process.stdout.write(progress.timemark);
       }).on('end', () => {
         fs.unlink(audioOutput, err => {
--- example/progress.js
+++ example/progress.js
@@ -16,7 +16,7 @@ video.on('progress', (chunkLength, downloaded, total) => {
   const percent = downloaded / total;
   const downloadedMinutes = (Date.now() - starttime) / 1000 / 60;
   readline.cursorTo(process.stdout, 0);
-  process.stdout.write(`${(percent * 100).toFixed(2)}% downloaded`);
+  process.stdout.write(`${(percent * 100).toFixed(2)}% downloaded `);
   process.stdout.write(`(${(downloaded / 1024 / 1024).toFixed(2)}MB of ${(total / 1024 / 1024).toFixed(2)}MB)\n`);
   process.stdout.write(`running for: ${downloadedMinutes.toFixed(2)}minutes`);
   process.stdout.write(`, estimated time left: ${(downloadedMinutes / percent - downloadedMinutes).toFixed(2)}minutes `);
