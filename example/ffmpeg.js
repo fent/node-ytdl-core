4ac44746ff6857a4dfc5a5770ed15cc26b5f6631
Aasim-A <saskiotchiha13@gmail.com>
2020-01-16 03:19:10 +0300

    Fixed the filter step for the audio and the video and changed the sound extension to m4a

--- example/ffmpeg.js
+++ example/ffmpeg.js
@@ -1,23 +1,30 @@
 // Install fluent-ffmpeg before running this!
 const readline = require('readline');
-const path   = require('path');
-const fs     = require('fs');
-const ytdl   = require('..');
+const path = require('path');
+const fs = require('fs');
 const ffmpeg = require('fluent-ffmpeg');
+const ytdl = require('..');
 
-const url = 'https://www.youtube.com/watch?v=TGbwL8kSpEk';
-const audioOutput = path.resolve(__dirname, 'sound.mp4');
+const url = 'https://www.youtube.com/watch?v=qgRUr-YUk1Q';
+const audioOutput = path.resolve(__dirname, 'sound.m4a');
 const mainOutput = path.resolve(__dirname, 'output.mp4');
 
 console.log('downloading audio track');
 ytdl(url, {
-  filter: format => format.container === 'm4a' && !format.encoding
-}).on('error', console.error)
-  .on('progress', (chunkLength, downloaded, total) => {
+  filter: format => format.container === 'mp4' && !format.qualityLabel
+})
+  .on('error', console.error)
+  .on('progress', (_, downloaded, total) => {
     const percent = downloaded / total;
     readline.cursorTo(process.stdout, 0);
     process.stdout.write(`${(percent * 100).toFixed(2)}% downloaded `);
-    process.stdout.write(`(${(downloaded / 1024 / 1024).toFixed(2)}MB of ${(total / 1024 / 1024).toFixed(2)}MB)`);
+    process.stdout.write(
+      `(${(downloaded / 1024 / 1024).toFixed(2)}MB of ${(
+        total /
+        1024 /
+        1024
+      ).toFixed(2)}MB)`
+    );
   })
 
   // Write audio to file since ffmpeg supports only one input stream.
@@ -25,8 +32,13 @@ ytdl(url, {
   .on('finish', () => {
     console.log('\ndownloading video');
     ffmpeg()
-      .input(ytdl(url, { filter: format => {
-        return format.container === 'mp4' && !format.audioEncoding; } }))
+      .input(
+        ytdl(url, {
+          filter: format => {
+            return format.container === 'mp4' && !format.audioBitrate;
+          }
+        })
+      )
       .videoCodec('copy')
       .input(audioOutput)
       .audioCodec('copy')
@@ -36,9 +48,10 @@ ytdl(url, {
         readline.cursorTo(process.stdout, 0);
         readline.clearLine(process.stdout, 1);
         process.stdout.write(progress.timemark);
-      }).on('end', () => {
+      })
+      .on('end', () => {
         fs.unlink(audioOutput, err => {
-          if(err) console.error(err);
+          if (err) console.error(err);
           else console.log('\nfinished downloading');
         });
       });
