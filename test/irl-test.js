e8a681b14fd898ad9d241edbbb1ef6417bb2e093
Aasim-A <23695024+Aasim-A@users.noreply.github.com>
2020-02-20 05:05:02 +0300

    Removed unnecessary field from irl-test videos

--- test/irl-test.js
+++ test/irl-test.js
@@ -12,7 +12,6 @@ let videos = {
   'Embed domain restricted' : 'B3eAMGXFw1o',
   'No embed allowed'        : 'GFg8BP01F5Q',
   'Offensive'               : 'hCKDsjLt_qU',
-  'No likes or dislikes'    : 'KKzOh0MRuZE',
 };
 
 
