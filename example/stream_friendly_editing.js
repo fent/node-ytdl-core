3aa2c25997baaac16fa617e4882e008ab7e91b7a
TimeForANinja <TimeForANinja@users.noreply.github.com>
2020-07-22 07:29:20 +0200

    Update stream_friendly_editing.js

--- example/stream_friendly_editing.js
+++ example/stream_friendly_editing.js
@@ -1,20 +1,20 @@
 const fs = require('fs');
 const ytdl = require('../');
 const cp = require('child_process');
-const { Duplex } = require('stream');
 const ffmpeg = require('ffmpeg-static');
 
 const ref = 'https://www.youtube.com/watch?v=aR-KAldshAE';
 
-/* reencode audio & video without creating files first */
+/* Reencode audio & video without creating files first */
 const audio = ytdl(ref, { filter: 'audioonly', quality: 'highestaudio' });
 const video = ytdl(ref, { filter: 'videoonly', quality: 'highestvideo' });
 
 
-const out = new Duplex();
 const process = cp.spawn(ffmpeg, [
-  '-loglevel', '0', '-hide_banner', // remove most logs
-  '-itsoffset', '3.0', '-i', 'pipe:3', // 3 second audio offset
+  // Remove most logs
+  '-loglevel', '0', '-hide_banner',
+  // 3 second audio offset
+  '-itsoffset', '3.0', '-i', 'pipe:3', 
   '-i', 'pipe:4',
   '-vf', 'scale=320:240',
   '-c:v', 'libx265', '-x265-params', 'log-level=0',
@@ -23,9 +23,9 @@ const process = cp.spawn(ffmpeg, [
 ], {
   windowsHide: true,
   stdio: [
-    /* stdin, stdout, stderr */
+    /* Standard: stdin, stdout, stderr */
     'inherit', 'inherit', 'inherit',
-    /* custom:3, custom:4, custom:5 */
+    /* Custom: Stream3, Stream4, Stream5 */
     'pipe', 'pipe', 'pipe',
   ],
 });
