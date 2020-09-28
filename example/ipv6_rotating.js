4ff3b8cbe39986c226e27e989896b85975f50ae3
Roki <35699619+Roki100@users.noreply.github.com>
2020-09-28 18:02:13 +0200

    linter got mad at missing semicolon nice

--- example/ipv6_rotating.js
+++ example/ipv6_rotating.js
@@ -6,7 +6,7 @@ let options = {
     quality: 'highest',
     chunking: true,
     IPv6Block: '2001:2::/48', // Example /48 block provided by https://www.iana.org/assignments/ipv6-unicast-address-assignments/ipv6-unicast-address-assignments.xhtml
-}
+};
 let url = 'https://www.youtube.com/watch?v=WhXefyLs-uw';
 let output = path.resolve(__dirname, 'video.mp4');
 
